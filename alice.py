e0be9138fe69e73dc8b7ee72e763c649ec917105
Fractal Feelings <kevin.mcisaac@gmail.com>
2023-05-21 14:02:17 +0800

    init

--- alice.py
+++ alice.py
@@ -1,6 +1,5 @@
 import pinecone 
 
-from langchain.llms import OpenAIChat
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
 from langchain.callbacks.base import BaseCallbackHandler
@@ -11,9 +10,9 @@ from langchain.vectorstores import Pinecone
 
 import streamlit as st
 
-from typing import Any, Dict, List
+from typing import Any
 import os
-from PIL import Image
+
 from dotenv import load_dotenv
 load_dotenv() #get API keys
 
@@ -37,30 +36,29 @@ def makeBot(k, streaming, namespace):
                         return_source_documents=True)
     return qa
 
-col1, mid, col2 = st.columns([15,1,100])
-col1.image('IBRS_Round.jpg', width=100)
-col2.title("Gideon's Playground")
-
-col2.caption("Use the inputs in the sidebar to experiment with Gideon's settings")
+st.title("Alice's Playground")
+st.caption("Use the inputs in the sidebar to experiment with the playground settings")
            
 with st.sidebar:
-    namespace = st.radio(os.environ.get('INDEX'), ('markdown-1200', 'article'), help = "For this database select the namespace for searching")
+    namespace = st.radio(os.environ.get('INDEX'), ('H2', 'H3', 'H5'), horizontal=True, 
+                         help = "For this database select the namespace for searching")
     streaming = st.radio("Stream answers?", ("Yes", "No"),horizontal=True, index=0,
                          help="Streaming shows the output as its generated like chatGPT but the cost and #tokens does not work. Turn off to see costs")
     
-    k = st.number_input("How many Articles should Gideon consult", value=5, 
+    k = st.number_input("How many Articles should Alice consult", value=5, 
                         min_value=1, max_value=50, 
                         help="This determines the number of similar article chunks to use in the prompt. Increasing this adds to the knowledge available but increases the cost/time to answer the inquiry")
     
-    instructions = st.text_area('Instructions for Gideon to answer the inquiry', 
-                                value='You are an experienced IBRS industry analyst speaking to an IT professional in this topic.\n\n'
+    instructions = st.text_area('Instructions for Alive to answer the inquiry', 
+                                value='You are an expert in Western Australia "Motor Vehicle (Third Party Insurance) Act"'
+                                      'answering questions from a citizen. \n\n'
                                       'Provide a clear, detailed answer based on the information provided below. Do not make up answers.\n\n'
                                       'If you do not know say "I do not know"',
                                 height = 300, 
                                 help='This is prepended to the inquiry and provides instructions on how to answer the inqueiry. Try chaning this to get a better result.')
     
 qa=makeBot(k, streaming, namespace) 
-inquiry = st.text_area(f"Hi, I'm Gideon, the IBRS Analyst Chatbot? What is your inquiry?",
+inquiry = st.text_area(f"Hi, I'm Alice, the ICWA Chatbot? What is your inquiry?",
                        value="")
 class SimpleStreamlitCallbackHandler(BaseCallbackHandler):
     """ Copied only streaming part from StreamlitCallbackHandler """
@@ -82,6 +80,6 @@ if inquiry:
             st.write(result['result']) 
         st.caption(f":blue[Cost {costs.total_cost*100 : 0.2f} US cents. Tokens {costs.total_tokens :,} = {costs.prompt_tokens :,} + {costs.completion_tokens :,}]")
 
-#    st.divider()
     for source in result["source_documents"]: 
-        st.markdown(f"<details><summary> {source.metadata.get('title')} ({source.metadata.get('category_name')})</summary>\n\n[Article]({source.metadata.get('URL')}) {source.metadata.get('published_date')}  \n\n{source.page_content} </details>", unsafe_allow_html=True)
+        st.markdown(f"<details><summary>{source.metadata.get('title')} </summary>{source.page_content} </details>", 
+                    unsafe_allow_html=True)
